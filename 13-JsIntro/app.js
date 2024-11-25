4657f8f4fd92af9f39049a73cf279dc10a970eef
Gurban Gurbanzada <gurban.gurbanzadeh@gmail.com>
2024-11-25 11:42:38 +0400

    js tutorial added

---
+++ 13-JsIntro/app.js
@@ -0,0 +1,140 @@
+"use strict";
+console.log("Hello World!");
+
+// let num1 = 5;
+// let num2 = 10;
+
+// let sum = num1 + num2;
+
+// console.log(sum);
+// var num2 = 10;
+
+// let num1 = 5;
+
+// let name = "gurban";
+
+// name = "kanan";
+
+// const num3 = 15;
+
+// num3 = 20;
+
+// console.log("iki ededin cemi", 5 + 3);
+// console.log("iki ededin ferqi", 5 - 3);
+// console.log("iki ededin ferqi", 5 * 3);
+// console.log("iki ededin ferqi", 5 / 3);
+
+// let eded = 8;
+
+// console.log("eded deyiseninin tipi-", typeof eded);
+
+// let name = "gurban";
+// name = "gurban";
+// name = `gurban`;
+// let surname = "gurbanzada";
+
+// console.log("name deyiseninin tipi-", typeof name);
+
+// console.log("fulname: ", name + " " + surname);
+
+// let num1 = 2;
+// let num2 = 6;
+// let sum = num1 + num2;
+
+// let result = `iki ededin cemi: ${num1}+${num2}=${sum}`;
+
+// console.log(result);
+
+let num = 3;
+
+// num = num + 4;
+
+// num += 4;
+
+// num++;
+// ++num;
+num++;
+
+num += 8;
+++num;
+
+num += "eli";
+
+// num = "eli";
+
+// console.log("num-", 3 * "gurban");
+
+// let result = true;
+// result = false;
+
+// console.log(3 > 5);
+// console.log(3 < 5);
+// console.log(5 <= 5);
+
+// let box;
+
+// console.log("box", box);
+
+// let item = null;
+
+// console.log("item", item);
+
+// let data = 1;
+
+// console.log(typeof data);
+// console.log(1);
+// console.log("salam");
+// console.log(true);
+
+// console.log(typeof 1);
+
+// false 0 "" null undefined NAN
+// let a = 9;
+// let b = 9;
+
+// if (a > b) {
+//   console.log("a ededi b den boyukdur");
+// } else if (a < b) {
+//   console.log("a ededi b den kicikdir");
+// } else {
+//   console.log("a ededi b ededine beraberdir");
+// }
+
+// alert("Xos gelmisiniz");
+
+// let info = prompt("adinizi daxil edin");
+
+// console.log("info-", info, "tipi", typeof info);
+
+// if (info * 1 == info) {
+//   console.log("ededdir");
+// } else {
+//   console.log("herfdir");
+// }
+
+// alert(`${info} Xos Gelmisiniz!`);
+
+// console.log(typeof info);
+
+// if (typeof info == "string") {
+//   console.log("daxil olunan info stringdir");
+// }else if(info==number)
+
+// let a = 6;
+// let b = "3";
+
+// console.log("result=", a / b);
+
+// let a = 6;
+
+// if (true === 1) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+// =
+
+// ==
+
+// ===
